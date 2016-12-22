3b16bb95af231561cbe3f66686dad9512e1513e0
oke <mail2oke@gmail.com>
2016-12-22 23:01:46 +0100

    login fix

--- app/www/views/login/login.js
+++ app/www/views/login/login.js
@@ -6,11 +6,5 @@ angular.module("App")
 	$scope.signup = function () {
 		$state.go("signup1");
 	};
-	$scope.$on('$ionicView.afterEnter', function(event) {
-    $ionicSideMenuDelegate.canDragContent(false);
-});
-//enable side menu drag before moving to next view
-$scope.$on('$ionicView.beforeLeave', function(event) {
-    $ionicSideMenuDelegate.canDragContent(true);
-});
+	
 })
